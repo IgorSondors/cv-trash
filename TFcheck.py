4987a15c735342a88d7d47181112483dbb49fd22
IgorSondors <44669029+IgorSondors@users.noreply.github.com>
2020-09-10 13:32:58 +0300

    Add files via upload

--- TFcheck.py
+++ TFcheck.py
@@ -4,3 +4,9 @@ print(tf.test.is_built_with_cuda())
 
 print(tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None))
 print(tf.__version__)
+
+if tf.test.is_built_with_cuda() and tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None):
+    print('You can use GPU')
+
+else:
+    print('TF can not use GPU')
